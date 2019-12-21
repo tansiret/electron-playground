6704f9370fc17f1dc11094a43d98e614d3b07ee5
yutyo <tanakinci2002@gmail.com>
2019-12-21 19:21:54 +0300

    hello-world added.

---
+++ hello world/main.js
@@ -0,0 +1,23 @@
+console.log('main project working');
+
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+
+let win;
+ function createWindow() {
+   win = new BrowserWindow();
+   win.loadURL(url.format({
+     pathname: path.join(__dirname, 'index.html'),
+     protocol: 'file',
+     slashes: true
+   }));
+
+   win.on('closed', () => {
+     win = null;
+   })
+ }
+
+ app.on('ready', createWindow);
